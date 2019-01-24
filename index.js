91c8f3a8200981af27df682af98c5336ede54955
Joshua Rivera-Sampayo <josh.rs1199@gmail.com>
2019-01-24 14:47:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,25 +1,7 @@
-function theBeatlesPlay(musicians, instruments) {
-  var music = [] // array
-  for (var i = 0; i < musicians.length; i++) {
-    var string = musicians[i] + " plays " + instruments[i];
-    music.push(string);
+function theBeatlesPlay(musicians, instruments){
+  var ss = [];
+  for (var i = 0; i < musicians.length; i++){
+    ss.push(musicians[i] + " plays " + instruments[i]);
   }
-  return music;
-}
-function johnLennonFacts(facts) {
-  var i = 0;
-   while (i < facts.length) {
-     facts[i] += "!!!";
-     i++;
-  }
-  return facts;
-}
-function iLoveTheBeatles(number){
-  var num = [];
-  do {
-    num.push("I love the Beatles!")
-    number++;
-  }
-  while (number <  15);
-  return num
+  return ss
 }
